f54ac62f01e9a2e70fd6d0e4f14adb175613e318
Charles Shames <47249885+cshames1@users.noreply.github.com>
2021-03-01 09:18:17 -0500

    Update Actions.js

--- js/Actions.js
+++ js/Actions.js
@@ -63,7 +63,7 @@ Actions.prototype.init = function()
 	this.addAction('saveSchematic', function() { ui.saveFile(); }, null, null, Editor.ctrlKey + '+S').isEnabled = isGraphEnabled;
 	this.addAction('rename...', function() { ui.renameFile(); }, null, null, null).isEnabled = isGraphEnabled;
 
-	this.addAction('importSchematic...', function() { ui.renameFile(); }, null, null, null).isEnabled = isGraphEnabled;
+	//this.addAction('importSchematic...', function() { ui.renameFile(); }, null, null, null).isEnabled = isGraphEnabled;
 	
 	action=this.addAction('editComponents...', mxUtils.bind(this, function()
 	{
